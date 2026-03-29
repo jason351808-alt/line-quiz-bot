6e4f5a5127a706e8978be44aca08cdb53111271d
jason351808-alt <jason351808@gmail.com>
2026-03-29 13:52:11 +0800

    Delete create_admin.py

--- create_admin.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-username = "admin"
-password = "1234"
-hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-
-print("請把下面 SQL 貼到 MySQL 執行：")
-print(f"INSERT INTO admin_users (username, password) VALUES ('{username}', '{hashed}');")
